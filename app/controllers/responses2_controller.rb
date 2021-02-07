b6c17d116a08805f5b249828e992bb42e1cc6077
Diego Philips <diego.r.philips@gmail.com>
2021-02-07 15:15:01 +0000

    changed endpoint

--- app/controllers/responses2_controller.rb
+++ app/controllers/responses2_controller.rb
@@ -1,10 +1,8 @@
 class Responses2Controller < ApplicationController
 
   def index
-    results = HTTParty.get("https://api.zotero.org/users/6431996/items/top", :headers =>
-    {"Authorization" => "Bearer #{ENV["ZOTERO_API_KEY"]}"},
-    "Content-Type" => "application/json")
-    resultsbody = results.body.encode('UTF-8', invalid: :replace, undef: :replace)
-    render json: resultsbody.to_json
+    results = HTTParty.get("https://api.zotero.org/users/6431996/items", :headers =>
+    {"Authorization" => "Bearer #{ENV["ZOTERO_API_KEY"]}"}, "Accept-Encoding" => "gzip, deflate, br", "Accept" => "*/*","Connection" => "keep-alive")
+    render json: results.body
   end
 end
