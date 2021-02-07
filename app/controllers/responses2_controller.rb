52a004b3a84fbda09f86bdce6c72e73f73857198
Diego Philips <diego.r.philips@gmail.com>
2021-02-07 12:31:55 +0000

    replaced unwanted characters in Zotero

--- app/controllers/responses2_controller.rb
+++ app/controllers/responses2_controller.rb
@@ -4,6 +4,7 @@ class Responses2Controller < ApplicationController
     results = HTTParty.get("https://api.zotero.org/users/6431996/items/top", :headers =>
     {"Authorization" => "Bearer #{ENV["ZOTERO_API_KEY"]}"},
     "Content-Type" => "application/json")
-    render json: results.to_json
+    resultsbody = results.body.encode('UTF-8', invalid: :replace, undef: :replace)
+    render json: resultsbody.to_json
   end
 end
