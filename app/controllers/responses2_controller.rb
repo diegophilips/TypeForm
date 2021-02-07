c6e2a962343145fcbc91efda2a5f876ba0b091ba
Diego Philips <diego.r.philips@gmail.com>
2021-02-07 15:50:28 +0000

    pushed limit to 100

--- app/controllers/responses2_controller.rb
+++ app/controllers/responses2_controller.rb
@@ -1,7 +1,7 @@
 class Responses2Controller < ApplicationController
 
   def index
-    results = HTTParty.get("https://api.zotero.org/users/6431996/items", :headers =>
+    results = HTTParty.get("https://api.zotero.org/users/6431996/items?limit=100", :headers =>
     {"Authorization" => "Bearer #{ENV["ZOTERO_API_KEY"]}"}, "Accept-Encoding" => "gzip, deflate, br", "Accept" => "*/*","Connection" => "keep-alive")
     render json: results.body
   end
