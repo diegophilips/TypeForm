c813fe58e6f6e971e8fe8d4f617633e9339eee09
Diego Philips <diego.r.philips@gmail.com>
2021-02-07 09:24:16 +0000

    created view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "responses#index"
+  root to: 'pages#home'
+  get :home, to: "pages#home"
   resources :responses, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
