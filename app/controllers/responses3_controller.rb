28a869d9c3fb4d0cf56a30ff6b5798021bf83762
Diego Philips <diego.r.philips@gmail.com>
2021-02-09 11:14:26 +0000

    changed return to Json

--- app/controllers/responses3_controller.rb
+++ app/controllers/responses3_controller.rb
@@ -2,7 +2,7 @@ class Responses3Controller < ApplicationController
 
   def index
      results = HTTParty.get("https://services.onetcenter.org/ws/mnm/careers/", :headers =>
-    {"Authorization" => "Basic #{ENV["ONET_KEY"]}"}, "Accept-Encoding" => "gzip, deflate, br", "Accept" => "*/*","Connection" => "keep-alive")
+    {"Authorization" => "Basic #{ENV["ONET_KEY"]}", "Accept" => "application/json"})
     render json: results.body
   end
 end
